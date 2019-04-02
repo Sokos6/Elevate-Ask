930c57f756bea00d6b145fe1b0af44c3cd0f88d7
Will Sokolowski <will.sokolowski@gmail.com>
2019-04-02 16:14:45 -0700

    nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <NavBar />
         <p>WIP</p>
       </div>
     );
